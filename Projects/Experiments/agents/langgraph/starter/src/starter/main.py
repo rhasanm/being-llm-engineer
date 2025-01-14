8eafd979297f998cd9a6bdcea640878960d0505c
rhasanm <hasanrakibul.masum@gmail.com>
2025-01-14 07:35:51 +0600

    feat: add memory checkpointer

--- Projects/Experiments/agents/langgraph/starter/src/starter/main.py
+++ Projects/Experiments/agents/langgraph/starter/src/starter/main.py
@@ -1,10 +1,32 @@
+import getpass
+import os
 from typing import Annotated
+import traceback
 
 from langgraph.graph import END, START, StateGraph
+from langgraph.graph.graph import RunnableConfig
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
 from typing_extensions import TypedDict
 
-from langchain_ollama import OllamaLLM
+from langchain_ollama import OllamaLLM, ChatOllama
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+def _set_env(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"{var}: ")
+
+_set_env("TAVILY_API_KEY")
+
+tool = TavilySearchResults(max_results=2)
+tools = [tool]
+
+llm = OllamaLLM(model="llama3.2:1b", base_url="http://localhost:11434")
+llm = ChatOllama(model="llama3.2:1b", base_url="http://localhost:11434")
+llm = ChatOllama(model="llama3.2:3b", base_url="http://localhost:11434")
+llm = ChatOllama(model="mistral", base_url="http://localhost:11434")
+llm = llm.bind_tools(tools)
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
@@ -12,17 +34,20 @@ class State(TypedDict):
 def chatbot(state: State):
     return {"messages": [llm.invoke(state["messages"])]}
 
-llm = OllamaLLM(model="llama3.2:1b", base_url="http://localhost:11434")
-
 graph_builder = StateGraph(State)
 
 graph_builder.add_node("chatbot", chatbot)
 
+tool_node = ToolNode(tools)
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges("chatbot", tools_condition)
+
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 
-graph = graph_builder.compile()
-
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
 
 try:
     image = graph.get_graph().draw_mermaid_png()
@@ -32,10 +57,10 @@ try:
 except Exception as e:
     print(f"Error {e} occurred")
 
+config: RunnableConfig = {"configurable": {"thread_id": "1"}}
 def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1])
+    for event in graph.stream({"messages": [("user", user_input)]}, config=config, stream_mode="values"):
+        event["messages"][-1].pretty_print()
 
 while True:
     try:
@@ -48,4 +73,5 @@ while True:
         stream_graph_updates(user_input)
     except Exception as e:
         print(f"Error {e} occurred")
+        traceback.print_exc()
         break
